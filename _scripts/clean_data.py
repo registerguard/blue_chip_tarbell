151aff7b68a81408d8bcc2d1c7fc0e36aa6de62c
John Heasly <jheasly@earthlink.net>
2018-07-03 17:49:26 -0700

    Keeping regex clean ups.

---
+++ _scripts/clean_data.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+import csv
+import re
+
+# https://codereview.stackexchange.com/questions/145511/performing-a-regex-search-and-saving-results-to-csv
+
+def main():
+    with open('RegisterGuardReports.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            addr = row['Site Address']
+            addr = re.sub(r' EUG$', '', addr, 1)
+            addr = re.sub(r'DR$', 'DRIVE', addr, 1)
+            addr = re.sub(r'LN$', 'LANE', addr, 1)
+            addr = re.sub(r' RD', ' ROAD', addr, 1)
+            addr = re.sub(r'LP$', 'LOOP', addr, 1)
+            addr = re.sub(r'PL$', 'PLACE', addr, 1)
+            addr = re.sub(r'ALY$', 'ALLEY', addr, 1)
+            addr = re.sub(r' BLDG ', ' BLDG. ', addr, 1)
+            addr = re.sub(r'(ST|AVE)$', r'\1.', addr)
+            addr = re.sub(r' ([NESW]) ', r' \1. ', addr, 1)
+            print('{}'.format(addr))
+
+if __name__ == "__main__":
+    main()
